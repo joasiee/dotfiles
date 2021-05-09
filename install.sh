7fde29fea67a7a3ec8748846a8984ebd84eec22d
Joas Mulder <joasmulder@hotmail.com>
2021-05-09 22:20:33 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install zsh
+sudo apt update && sudo apt upgrade -y && sudo apt install zsh stow curl
+
+git config --global user.email "joasmulder@hotmail.com"
+git config --global user.name "Joas Mulder"
+git clone https://github.com/joasiee/dotfiles.git
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cd dotfiles
+stow zsh
