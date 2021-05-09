15d0a8393e97ad57bbd44fc76c87607aa166299f
Joas Mulder <joasmulder@hotmail.com>
2021-05-09 22:50:50 +0200

    default shell zsh

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ git config --global user.name "Joas Mulder"
 git clone https://github.com/joasiee/dotfiles.git
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+chsh -s "$zsh"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 rm .zshrc
