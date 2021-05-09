0205c74b4c16c86a56a761da2f4e117906b48289
Joas Mulder <joasmulder@hotmail.com>
2021-05-09 22:49:30 +0200

    unattended

--- install.sh
+++ install.sh
@@ -14,11 +14,10 @@ git config --global user.email "joasmulder@hotmail.com"
 git config --global user.name "Joas Mulder"
 git clone https://github.com/joasiee/dotfiles.git
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 rm .zshrc
-rm .p10k.zsh
 
 cd dotfiles
 stow zsh
