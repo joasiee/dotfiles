280ce28fd5f20d429d142d5dbc2e99bd74b3148b
Joas Mulder <joasmulder@hotmail.com>
2021-05-09 22:33:45 +0200

    p10k zsh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ git config --global user.email "joasmulder@hotmail.com"
 git config --global user.name "Joas Mulder"
 git clone https://github.com/joasiee/dotfiles.git
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 cd dotfiles
 stow zsh
