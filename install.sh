b8c117a5f2bed1f853fc435c7c911105a9fdf48d
Joas Mulder <joasmulder@hotmail.com>
2021-05-10 20:22:18 +0200

    update

--- install.sh
+++ install.sh
@@ -4,18 +4,16 @@ cd $HOME
 OS=$NAME
 
 case "$OS" in
-Ubuntu) sudo apt update && sudo apt upgrade -y && sudo apt install zsh stow
+Ubuntu) sudo apt update && sudo apt upgrade -y && sudo apt install zsh stow -y
         ;;
-Fedora) sudo dnf upgrade -y && sudo dnf install zsh stow curl -y
+Fedora) sudo dnf upgrade -y && sudo dnf install zsh stow -y
         ;;
 esac
 
-git config --global user.email "joasmulder@hotmail.com"
-git config --global user.name "Joas Mulder"
 git clone https://github.com/joasiee/dotfiles.git
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-chsh -s "$zsh"
+chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 rm .zshrc
