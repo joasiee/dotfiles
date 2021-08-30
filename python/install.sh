33c92f498f2ee59c6ad6bc03ac48992fb4eb5c1a
joasiee <joasmulder@hotmail.com>
2021-08-31 01:58:12 +0200

    Update install.sh

--- python/install.sh
+++ python/install.sh
@@ -1,7 +1,7 @@
 VERSION="3.9.7"
 
 sudo apt update
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev -y
 wget https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz
 tar -xf Python-$VERSION.tgz
 cd Python-$VERSION
