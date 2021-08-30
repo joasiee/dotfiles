2a45f91a2ab2d7adaf4207738f4353bedc562bcf
joasiee <joasmulder@hotmail.com>
2021-08-31 01:55:25 +0200

    Create install.sh

---
+++ python/install.sh
@@ -0,0 +1,12 @@
+VERSION="3.9.7"
+
+sudo apt update
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+wget https://www.python.org/ftp/python/3.9.7/Python-$VERSION.tgz
+tar -xf Python-$VERSION.tgz
+cd Python-$VERSION
+./configure --enable-optimizations
+make -j 2
+sudo make altinstall
+cd ..
+rm --dir -f -R Python-$VERSION
