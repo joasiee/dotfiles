e74a483d92b0f4a86d4920c9f4f8201082ad765f
joasiee <joasmulder@hotmail.com>
2021-11-17 05:28:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,5 +21,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+rm .zshrc
+
 cd dotfiles
 stow zsh
