5218588879201c9bc036d3fe9760e574ef42e757
joasiee <joasmulder@hotmail.com>
2021-11-17 05:27:12 +0100

    add arch

--- install.sh
+++ install.sh
@@ -1,24 +1,25 @@
 cd $HOME
 
-. /etc/os-release
-OS=$ID
+if command -v apt &> /dev/null
+then
+    sudo apt update && sudo apt install git zsh stow curl keychain -y
+fi
 
-case "$OS" in
-ubuntu) sudo apt update && sudo apt install git zsh stow curl keychain -y
-        ;;
-debian) sudo apt update && sudo apt install git zsh stow curl keychain -y
-        ;;
-fedora) sudo dnf upgrade -y && sudo dnf install git zsh stow curl keychain -y
-        ;;
-esac
+if command -v dnf &> /dev/null
+then
+    sudo dnf upgrade -y && sudo dnf install git zsh stow curl keychain -y
+fi
+
+if command -v pacman &> /dev/null
+then
+    sudo pacman -Syu --noconfirm git zsh stow curl keychain -y
+fi
 
 git clone https://github.com/joasiee/dotfiles.git
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-rm .zshrc
 
 cd dotfiles
 stow zsh
