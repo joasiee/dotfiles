109d6a4122ada83a52fa40ea61ca5e0702827174
Joas Mulder <joasmulder@hotmail.com>
2022-02-27 11:37:32 +0100

    pipx poetry pyenv added to installation script

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@ cd $HOME
 
 if command -v apt &> /dev/null
 then
-    sudo apt update && sudo apt install git zsh stow curl keychain -y
+    sudo apt update && sudo apt install git zsh stow curl keychain python3-pip -y
 fi
 
 if command -v dnf &> /dev/null
 then
-    sudo dnf upgrade -y && sudo dnf install git zsh stow curl keychain -y
+    sudo dnf upgrade -y && sudo dnf install git zsh stow curl keychain python3-pip -y
 fi
 
 if command -v pacman &> /dev/null
 then
-    sudo pacman -Syu --noconfirm git zsh stow curl keychain -y
+    sudo pacman -Syu --noconfirm git zsh stow curl keychain python-pip -y
 fi
 
 git clone https://github.com/joasiee/dotfiles.git
@@ -20,8 +20,13 @@ git clone https://github.com/joasiee/dotfiles.git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 rm .zshrc
+
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+pipx install poetry
+curl https://pyenv.run | bash
 
 cd dotfiles
 stow zsh
+exec $SHELL
